ef28e02020aaaca24ff410bdbd100e9b18e22ac8
Diego Quan <dquan@delicatessen502.com>
2025-07-17 08:45:53 -0600

    Ejercicio 2

--- src/Main.kt
+++ src/Main.kt
@@ -13,12 +13,13 @@ fun calcularPromedio(numeros: List<Double>): Double{
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Función de promedio de números")
+    println("")
     val lista = listOf(12.3,45.2,15.4,11.1)
     val promedio = calcularPromedio(lista)
     println("El promedio de los números es: $promedio")
 
     println("")
-    println("Números impares")
     val numeros = listOf(1,4,2,6,8,3,7,34,78,22,43,65,23)
+    val impares = numeros.filter {it % 2 == 1}
+    println("Números impares: $impares")
 }
