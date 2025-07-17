a36ec03a5e988434e1653573bd22fc9e42ff8197
Diego Quan <dquan@delicatessen502.com>
2025-07-17 08:41:05 -0600

    Ejercicio 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,24 @@
+//Autor: Diego QUan
+//Actividad: Laboratorio 2
+//Fecha: 17/07/2025
+
+//Función para calcular el promedio
+fun calcularPromedio(numeros: List<Double>): Double{
+    if (numeros.isEmpty()){
+        throw IllegalArgumentException("La lista no puede estar vacía")
+    }
+    val suma = numeros.reduce{a, b -> a + b}
+    return suma / numeros.size
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Función de promedio de números")
+    val lista = listOf(12.3,45.2,15.4,11.1)
+    val promedio = calcularPromedio(lista)
+    println("El promedio de los números es: $promedio")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("")
+    println("Números impares")
+    val numeros = listOf(1,4,2,6,8,3,7,34,78,22,43,65,23)
 }
